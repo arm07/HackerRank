fb4592f465a3aba0114e0c990c1ed9efc2cb56f2
arm07 <rallam2@uic.edu>
2017-04-12 13:40:11 -0500

    2dArray

---
+++ 2DArray.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int arr[][] = new int[6][6];
+        int maxSum=-100;
+        for(int i=0; i < 6; i++){
+            for(int j=0; j < 6; j++){
+                arr[i][j] = in.nextInt();
+            }
+        }
+        
+        for(int i=0; i < 4; i++){
+            for(int j=0; j < 4; j++){
+                int currSum=arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
+                if(currSum>maxSum)
+                    maxSum=currSum;
+                
+            }
+        }
+        System.out.println(maxSum);
+    }
+}
